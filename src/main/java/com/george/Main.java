97ac6903bcac7b843641499998a3f04d758e267f
plasticGeorge <georgylemko@yandex.ru>
2021-06-12 14:30:28 +0300

    fuck...

--- src/main/java/com/george/Main.java
+++ src/main/java/com/george/Main.java
@@ -4,18 +4,20 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException, LifecycleException {
-        String contextPath = "/";
-        String appsBase = ".";
+        String contextPath = "";
+        String docBase = new File(".").getAbsolutePath();
+
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
-        Context context = tomcat.addContext(contextPath, appsBase);
-        Tomcat.addServlet(context, "PersonServlet", new PersonServlet());
 
-        context.addServletMappingDecoded("/*", "PersonServlet");
+        Context context = tomcat.addContext(contextPath, docBase);
+        tomcat.addServlet(contextPath, "PersonServlet", new PersonServlet());
+        context.addServletMappingDecoded("/person", "PersonServlet");
 
         tomcat.start();
         tomcat.getServer().await();
